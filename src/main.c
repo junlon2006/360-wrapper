d6ca5271d32e1709c53d908c0dd6b8ffcd109bf6
shangjinlong <shangjinlong@unisound.com>
2018-08-09 16:38:45 +0800

    Format code

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@ static void _get_audio_source(char *raw_pcm, int bytes_len,
                               LasrResult *result, int vad_end) {
   if (result) {
     LOGT(MAIN_TAG, "get asr result. keyword=%s, keyword_audio=%p, "
-         "keyword_audio_len=%d, raw_audio_len=%d", result->keyword, result->audio_contain_keyword,
+         "keyword_audio_len=%d, raw_audio_len=%d", result->keyword,
+         result->audio_contain_keyword,
          result->audio_len, bytes_len);
   }
   if (vad_end && NULL == result) {
@@ -47,7 +48,7 @@ static void _get_audio_source(char *raw_pcm, int bytes_len,
 int main() {
   LasrParam lasr_param;
   lasr_param.frame_size_msec = 50;
-  if (0 != LasrInit(RESOURCE_PATH, &lasr_param, (CbAudioSource)_get_audio_source)) {
+  if (0 != LasrInit(RESOURCE_PATH, &lasr_param, _get_audio_source)) {
     LOGE(MAIN_TAG, "lasr init failed");
     return -1;
   }
