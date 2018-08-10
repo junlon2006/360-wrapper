6647f5081e6ea6745d20289446a51c799dcc1850
shangjinlong <shangjinlong@unisound.com>
2018-08-10 21:45:58 +0800

    Add some test code

--- src/main.c
+++ src/main.c
@@ -22,18 +22,19 @@
  *
  **********************************************************************/
 
-
 #include "../inc/uni_lasr.h"
 #include "uni_log.h"
 #include "uni_iot.h"
 #include "uni_types.h"
 #include "uni_audio_player.h"
 #include "uni_mp3_player.h"
+#include "uni_tts_player.h"
+#include "uni_config.h"
 #include <stdio.h>
 #include <unistd.h>
 
 #define MAIN_TAG        "main"
-#define RESOURCE_PATH   "/tmp/test/uniapp"
+#define RESOURCE_PATH   "/run/uniapp"
 
 typedef enum {
   MUSIC_PLAYING,
@@ -44,7 +45,7 @@ typedef enum {
 static void _music_test(char *command) {
   static MusicState state = MUSIC_IDLE;
   if (0 == strcmp(command, "我要听歌")) {
-    LOGW(MAIN_TAG, "play music.....");
+    LOGW(MAIN_TAG, "play music");
     if (MUSIC_IDLE == state) {
       Mp3Play("http://m128.xiami.net/158/7158/2103755363/180336"
               "5859_1529979280707.mp3?auth_key=1534474800-0-0-d84"
@@ -56,7 +57,7 @@ static void _music_test(char *command) {
     return;
   }
   if (0 == strcmp(command, "停止播放")) {
-    LOGW(MAIN_TAG, "stop music.....");
+    LOGW(MAIN_TAG, "stop music");
     if (state != MUSIC_IDLE) {
       Mp3Stop();
       state = MUSIC_IDLE;
@@ -66,7 +67,7 @@ static void _music_test(char *command) {
     return;
   }
   if (0 == strcmp(command, "继续播放")) {
-    LOGW(MAIN_TAG, "resume music...");
+    LOGW(MAIN_TAG, "resume music");
     if (state == MUSIC_PAUSE) {
       Mp3Resume();
       state = MUSIC_PLAYING;
@@ -82,7 +83,7 @@ static void _music_test(char *command) {
     } else {
       LOGW(MAIN_TAG, "invalid state");
     }
-    LOGW(MAIN_TAG, "pause music...");
+    LOGW(MAIN_TAG, "pause music");
     return;
   }
 }
@@ -121,15 +122,32 @@ static Result _audio_out_init() {
 int main() {
   LasrParam lasr_param;
   lasr_param.frame_size_msec = 50;
+  LogInitialize(NULL);
+  ConfigInitialize();
   _audio_out_init();
+  TtsCreate(RESOURCE_PATH"/config/config_file");
   if (0 != LasrInit(RESOURCE_PATH, &lasr_param, _get_audio_source)) {
     LOGE(MAIN_TAG, "lasr init failed");
     return -1;
+  }
+  usleep(1000 * 3000);
+  int i = 0;
+  while (1) {
+    if (i++ % 2 == 1) {
+      TtsSetSpeaker("sweet");
+    } else {
+      TtsSetSpeaker("lzl");
+    }
+    TtsPlayString("欢迎使用云知声智能语音产品", TTS_ONLINE);
+    usleep(1000 * 10000);
   }
   LOGT(MAIN_TAG, "360 demo start successfully");
   while (1) {
     usleep(1000 * 1000);
   }
   LasrFinal();
+  TtsDestroy();
+  ConfigFinalize();
+  LogFinalize();
   return 0;
 }
