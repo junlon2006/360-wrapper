ac2346b7efcdad594897dc89d7a3fa87540af775
shangjinlong <shangjinlong@unisound.com>
2018-08-13 11:32:05 +0800

    Format code

--- src/main.c
+++ src/main.c
@@ -47,19 +47,18 @@ static void _music_test(char *command) {
   if (0 == strcmp(command, "小宝小宝")) {
     LOGT(MAIN_TAG, "tts speaker: boy");
     TtsSetSpeaker("boy");
-    if (state == MUSIC_IDLE) {
-      TtsPlayString("你好，我在", TTS_ONLINE);
-    }
+    TtsPlayString("你好，我在", TTS_ONLINE);
+    AudioPlayerSetFrontType(AUDIO_TTS_PLAYER, 0.8);
     return;
   }
   if (0 == strcmp(command, "小贝小贝")) {
     LOGT(MAIN_TAG, "tts speaker: lzl");
-    TtsSetSpeaker("lzl");
-    if (state == MUSIC_IDLE) {
-      TtsPlayString("你好，我在", TTS_ONLINE);
-    }
+    TtsSetSpeaker("kiyo");
+    TtsPlayString("你好，我在", TTS_ONLINE);
+    AudioPlayerSetFrontType(AUDIO_TTS_PLAYER, 0.8);
     return;
   }
+  LasrWakeupReset();
   if (0 == strcmp(command, "我要听歌")) {
     LOGW(MAIN_TAG, "play music");
     if (MUSIC_IDLE == state) {
@@ -106,7 +105,6 @@ static void _music_test(char *command) {
     TtsPlayString("北京今天天气晴，33到37度，空气质量优，外出请注意安全",
                   TTS_ONLINE);
     AudioPlayerSetFrontType(AUDIO_TTS_PLAYER, 0.8);
-    //LasrWakeupReset();
     return;
   }
 }
